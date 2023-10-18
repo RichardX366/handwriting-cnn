aba34c51d4e32cf5a4bf2a7e5e49c4e1446bb537
Richard X <richardx366@gmail.com>
2023-10-18 19:56:10 -0400

    serial

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-// navigator.serial
-//   .requestPort()
-//   .then((port) => {
-//     console.log(port);
-//   })
-//   .catch((e) => {
-//     console.log(e);
-//   });
+const encoder = new TextEncoder();
+const textDecoder = new TextDecoderStream();
+const reader = textDecoder.readable.getReader();
+
+const pickSerial = async () => {
+  try {
+    const pico = await navigator.serial.requestPort();
+    await pico.open({ baudRate: 115200 });
+    pico.readable.pipeTo(textDecoder.writable);
+
+    const writer = pico.writable.getWriter();
+
+    const send = (str) => writer.write(encoder.encode(str));
+
+    document.querySelector('#overlay').style.display = 'none';
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+document.querySelector('#serial').onclick = pickSerial;
 
 const write = () => {
   window.run(
